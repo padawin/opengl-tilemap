cfb275bf403f3f99720524d5003f2fb6494d1a4a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-03-20 16:59:33 +0100

    Camera moved back

--- src/tilemap/scene/Game.cpp
+++ src/tilemap/scene/Game.cpp
@@ -15,7 +15,7 @@ GameScene::GameScene(UserActions &userActions) :
 
 bool GameScene::onEnter() {
 	m_reference = std::shared_ptr<GameObject>(new GameObject);
-	setCameraView(std::shared_ptr<CameraView>(new FollowView(m_reference, glm::vec3(0.0f, 0.0f, 1.0f))));
+	setCameraView(std::shared_ptr<CameraView>(new FollowView(m_reference, glm::vec3(0.0f, 0.0f, 20.0f))));
 
 	setCamera(std::shared_ptr<Camera>(new PerspectiveCamera(m_cameraView, 45.0f, 800.0f / 600.0f, 0.1f, 100.0f)));
 	return true;
