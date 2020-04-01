fcdcbcb554ce6a7d21cfa744a32647b19df9e9a7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-04-01 19:45:16 +0200

    Type named in a more accurate way

--- src/tilemap/TilemapReader.hpp
+++ src/tilemap/TilemapReader.hpp
@@ -5,7 +5,7 @@
 #include <map>
 #include <vector>
 
-typedef enum {SIZE, LAYER_START, LAYER_ATLAS, LAYER, COLLISION_LAYER, END_LAYER} ProcessStep;
+typedef enum {SIZE, LAYER_START, LAYER_ATLAS, LAYER, COLLISION_LAYER, END_LAYER} TilemapParseStep;
 
 typedef struct {
 	std::map<const char*, unsigned int> textures = {};
@@ -22,7 +22,7 @@ typedef struct {
 
 class TilemapReader {
 	private:
-	ProcessStep m_currentStep = SIZE;
+	TilemapParseStep m_currentStep = SIZE;
 
 	public:
 	bool process(std::string filePath, TilemapFileFormat &data);
